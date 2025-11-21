5a9c569a6072ba27c8db6e5c10e1e8f1f0e4108f
robertpniower <robert-pniower@gmx.de>
2025-11-21 16:12:23 +0100

    challange completed

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,33 @@
+class ListsController < ApplicationController
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+    # @bookmark = Bookmark.find(@list.id)
+    @bookmark_new = Bookmark.new
+    @movies = @list.movies
+
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.save
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render "lists/new", status: :unprocessable_entity
+    end
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+  end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,83 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'uri'
+require 'net/http'
+require 'json'
+require 'faker'
+
+
+puts "Cleaning database..."
+Bookmark.destroy_all
+List.destroy_all
+Movie.destroy_all
+
+puts "Fetching and seeding movies from TMDB (Top Rated)..."
+
+url = URI("https://tmdb.lewagon.com/movie/top_rated")
+
+http = Net::HTTP.new(url.host, url.port)
+http.use_ssl = true
+
+request = Net::HTTP::Get.new(url)
+request["accept"] = 'application/json'
+
+response = http.request(request)
+
+data_hash = JSON.parse(response.body)
+results = data_hash["results"]
+
+results.each do |movie_data|
+  Movie.create!(
+    title: movie_data["title"],
+    overview: movie_data["overview"],
+    rating: movie_data["vote_average"],
+
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie_data["poster_path"]}"
+  )
+end
+
+puts "Seeded #{Movie.count} movies."
+puts "-----------------------------------"
+
+
+
+puts "Creating 4 Lists..."
+list_names = ["Must Watch", "Guilty Pleasures", "Family Night", "Sci-Fi Favorites"]
+lists = []
+
+list_names.each do |name|
+  list = List.create!(name: name)
+  lists << list
+  puts "Created list: #{name}"
+end
+
+puts "-----------------------------------"
+
+puts "Creating Bookmarks (4-6 movies per list)..."
+
+
+all_movies = Movie.all
+
+
+if all_movies.count < 10
+  puts "Warning: Not enough movies fetched to fully populate all lists."
+end
+
+lists.each do |list|
+
+  num_movies_to_bookmark = rand(4..6)
+
+
+  movies_for_list = all_movies.sample(num_movies_to_bookmark)
+
+  movies_for_list.each do |movie|
+    Bookmark.create!(
+      comment: Faker::Lorem.sentence(word_count: 5, supplemental: true),
+      movie: movie,
+      list: list
+    )
+    puts "Bookmarked '#{movie.title.truncate(20)}' to list '#{list.name}'"
+  end
+end
+
+puts "--- Seeding Complete ---"
+puts "Total Lists: #{List.count}"
+puts "Total Bookmarks: #{Bookmark.count}"
